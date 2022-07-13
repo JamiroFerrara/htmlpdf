c5391c1c64bd006e8709ee4b458bc6d497731546
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-07-13 14:16:13 +0200

    Update

--- src/pages/api/getOffers.js
+++ src/pages/api/getOffers.js
@@ -1,6 +1,7 @@
 import * as sr from '../../server/signalr'
 
 export default async function handler(req, res) {
+  console.log("getOffersLog!")
   await sr.InitConnection();
   const offers = await sr.GetOffers();
   res.status(200).json({ offers })
