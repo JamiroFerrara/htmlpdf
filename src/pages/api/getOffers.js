e4c07296f6929449aa0b09977d1458e009ed3d8f
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-07-13 12:24:31 +0200

    ..

--- src/pages/api/getOffers.js
+++ src/pages/api/getOffers.js
@@ -1,27 +1,6 @@
 import * as sr from '../../server/signalr'
-import Cors from 'cors'
-
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
 
 export default async function handler(req, res) {
-  await runMiddleware(req, res, cors)
   await sr.InitConnection();
   const offers = await sr.GetOffers();
   res.status(200).json({ offers })
