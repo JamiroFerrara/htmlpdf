ad14c01274e35721a4a308f77e4f45dd2eb9fa17
Jamie <83338941+JamiroFerrara@users.noreply.github.com>
2022-07-13 15:10:00 +0200

    Update

--- src/pages/api/getOffers.js
+++ src/pages/api/getOffers.js
@@ -1,41 +1,10 @@
-import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import * as sr from '../../server/signalr'
 const ws = require('ws');
 const eventsource = require('eventsource');
 
-let connection;
-
 export default async function handler(req, res) {
   console.log("getOffersLog!")
-  await InitConnection();
-  const offers = await GetOffers();
+  await sr.InitConnection();
+  const offers = await sr.GetOffers();
   res.status(200).json({ offers })
-}
-
-
-export async function InitConnection(){
-  const ip = "signalr.tecnoace.eu"
-  // const iIp = "192.168.11.54"
-  const port = "15191"
-  const hub  = "Tecnoace_Report"
-  const url = "https://" + ip + ":" + port + "/" + hub
-
-  try {
-    connection = new HubConnectionBuilder()
-      .withUrl(url)
-      .configureLogging(LogLevel.Information)
-      .build();
-
-    // connection.onclose(e => {
-    // });
-
-    await connection.start().then(() => {console.log("Connected!")});
-    // await connection.invoke("SendMessage", "phone", "connected" );
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-export async function GetOffers(){
-  const res = await connection.invoke("GetOffers", false)
-  return res;
 }
